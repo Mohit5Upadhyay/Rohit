b3ea384e8fa5fa40ec3c9a8d93c214e98171a79f
Anuj Upadhyay <anujupadhyay@Anujs-MacBook-Air.local>
2025-01-12 22:40:27 +0530

    added vercel speed insight

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -299,7 +299,7 @@ import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../appwrite/auth";
 import { useNewsletter } from "../hooks/useNewsletter";
 import { toast, Toaster } from 'react-hot-toast';
-
+import { Analytics} from '@vercel/analytics/react';
 
 
 // Banner mapping
@@ -607,6 +607,7 @@ function Layout() {
           </p>
         </div>
       </footer>
+      <Analytics/>
     </div>
   );
 }
